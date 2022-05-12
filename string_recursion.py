c0283d085060093c41448f23a0061f86a176db56
DrHrast <92861520+DrHrast@users.noreply.github.com>
2022-05-12 08:46:00 +0200

    Update string_recursion.py

--- string_recursion.py
+++ string_recursion.py
@@ -2,9 +2,9 @@ def rec_string(text, word, index = 0, counter = 0):
     if len(text) - index <= 1:
         print(f"U tekstu se nalazi {counter} puta riječ '{word}'.")
         return
-    elif text[index] + text[index + 1] == word:
+    elif text[index:index + len(word)] == word:
         counter += 1
-        rec_string(text, word, index + 2, counter)
+        rec_string(text, word, index + len(word), counter)
     else:
         rec_string(text, word, index + 1, counter)
 
